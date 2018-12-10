ee80b458f7f69693fc1eb518aa89f9ce1125f332
Jan Bujak <jan.bujak@nokia.com>
2018-12-10 08:00:51 +0100

    Improved debug info search heuristics

--- nwind/src/debug_info_index.rs
+++ nwind/src/debug_info_index.rs
@@ -5,8 +5,13 @@ use std::path::Path;
 use binary::BinaryData;
 
 pub struct DebugInfoIndex {
-    by_filename: HashMap< Vec< u8 >, Arc< BinaryData > >,
-    by_build_id: HashMap< Vec< u8 >, Arc< BinaryData > >
+    by_filename: HashMap< Vec< u8 >, Vec< Arc< BinaryData > > >,
+    by_build_id: HashMap< Vec< u8 >, Vec< Arc< BinaryData > > >
+}
+
+fn check_build_id< 'a >( data: &'a Arc< BinaryData >, expected_build_id: Option< &[u8] > ) -> bool {
+    let build_id = data.build_id();
+    expected_build_id.is_none() || build_id.is_none() || build_id == expected_build_id
 }
 
 impl DebugInfoIndex {
@@ -22,27 +27,63 @@ impl DebugInfoIndex {
     }
 
     pub fn get( &self, basename: &str, debuglink: Option< &[u8] >, build_id: Option< &[u8] > ) -> Option< &Arc< BinaryData > > {
+        let (bin, dbg) = self.get_pair( basename, debuglink, build_id );
+        dbg.or( bin )
+    }
+
+    pub fn get_pair( &self, basename: &str, debuglink: Option< &[u8] >, build_id: Option< &[u8] > ) -> (Option< &Arc< BinaryData > >, Option< &Arc< BinaryData > >) {
         let basename: &[u8] = basename.as_ref();
-        if let Some( debuglink ) = debuglink {
-            if let Some( data ) = self.by_filename.get( debuglink ) {
-                if build_id == data.build_id() {
-                    return Some( data );
+
+        let mut candidates = Vec::new();
+        if let Some( build_id ) = build_id {
+            if let Some( entries ) = self.by_build_id.get( build_id ) {
+                candidates.extend( entries );
+
+                for entry in entries {
+                    if let Some( debuglink ) = entry.debuglink() {
+                        if let Some( debug_entries ) = self.by_filename.get( debuglink ) {
+                            candidates.extend( debug_entries.iter().filter( |data| check_build_id( data, Some( build_id ) ) ) );
+                        }
+                    }
                 }
             }
         }
 
-        let data = self.by_filename.get( basename )?;
-        if build_id == data.build_id() {
-            return Some( data );
-        }
+        if let Some( entries ) = self.by_filename.get( basename ) {
+            candidates.extend( entries.iter().filter( |data| check_build_id( data, build_id ) ) );
 
-        if let Some( build_id ) = build_id {
-            if let Some( data ) = self.by_build_id.get( build_id ) {
-                return Some( data );
+            for entry in entries {
+                if let Some( debuglink ) = entry.debuglink() {
+                    if let Some( debug_entries ) = self.by_filename.get( debuglink ) {
+                        candidates.extend( debug_entries.iter().filter( |data| check_build_id( data, build_id ) ) );
+                    }
+                }
             }
         }
 
-        None
+        if let Some( debuglink ) = debuglink {
+            if let Some( entries ) = self.by_filename.get( debuglink ) {
+                candidates.extend( entries.iter().filter( |data| check_build_id( data, build_id ) ) );
+            }
+        }
+
+        candidates.sort_by_key( |entry| entry.as_ptr() );
+        candidates.dedup_by_key( |entry| entry.as_ptr() );
+        let matching: Vec< _ > = candidates.iter().filter( |entry| entry.build_id().is_some() && entry.build_id() == build_id ).cloned().collect();
+        if !matching.is_empty() {
+            candidates = matching;
+        }
+
+        match candidates.len() {
+            0 => return (None, None),
+            1 => return (candidates.pop(), None),
+            _ => {
+                candidates.sort_by_key( |entry| entry.as_bytes().len() );
+                let dbg = candidates.pop();
+                let bin = candidates.pop();
+                (bin, dbg)
+            }
+        }
     }
 
     fn add_impl( &mut self, path: &Path ) {
@@ -74,9 +115,9 @@ impl DebugInfoIndex {
             Ok( binary ) => {
                 let filename = path.file_name().unwrap();
                 let binary = Arc::new( binary );
-                self.by_filename.insert( filename.to_string_lossy().into_owned().into_bytes(), binary.clone() );
+                self.by_filename.entry( filename.to_string_lossy().into_owned().into_bytes() ).or_default().push( binary.clone() );
                 if let Some( build_id ) = binary.build_id() {
-                    self.by_build_id.insert( build_id.to_owned(), binary.clone() );
+                    self.by_build_id.entry( build_id.to_owned() ).or_default().push( binary.clone() );
                 }
             },
             Err( error ) => {
