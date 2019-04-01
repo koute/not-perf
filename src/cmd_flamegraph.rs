2e28c58c7af8b25ce565071f5387497529060af0
Jan Bujak <jan.bujak@nokia.com>
2019-04-01 14:40:45 +0200

    Add missing `src/cmd_flamegraph.rs`

---
+++ src/cmd_flamegraph.rs
@@ -0,0 +1,18 @@
+use std::error::Error;
+use std::io;
+
+use inferno::flamegraph;
+
+use args;
+use cmd_collate::collapse_into_sorted_vec;
+
+pub fn main( args: args::FlamegraphArgs ) -> Result< (), Box< Error > > {
+    let lines = collapse_into_sorted_vec( &args.collation_args )?;
+    let mut options = flamegraph::Options::default();
+
+    let stdout = io::stdout();
+    let stdout = stdout.lock();
+    flamegraph::from_sorted_lines( &mut options, lines.iter().map( |line| line.as_str() ), stdout ).unwrap();
+
+    Ok(())
+}
