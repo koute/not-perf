a678fb459e0377ef39e5eb518df4cf3779b52dcd
Jan Bujak <jan.bujak@nokia.com>
2019-04-26 13:17:16 +0200

    Test `aarch64-unknown-linux-gnu` compilation on Travis

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -31,5 +31,7 @@ cargo build -p nperf --verbose
 cargo test -p nperf --bin nperf --verbose
 rustup target add mips64-unknown-linux-gnuabi64
 rustup target add armv7-unknown-linux-gnueabihf
+rustup target add aarch64-unknown-linux-gnu
 cargo check --target=mips64-unknown-linux-gnuabi64
 cargo check --target=armv7-unknown-linux-gnueabihf
+cargo check --target=aarch64-unknown-linux-gnu
